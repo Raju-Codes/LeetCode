291de747b40d91e543591275a97f0fb23b03d138
Raju <rajub.codes@gmail.com>
2026-01-09 22:13:25 +0530

    Time: 5 ms (2.31%) | Memory: 52.7 MB (6.94%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -4,11 +4,13 @@ class Solution {
         
         for(int i = 1; i<=n; i++){
             String con = "";
-            if(i%3==0 && i%5==0) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if(divisibleBy3 && divisibleBy5) {
                 con = "FizzBuzz";
-            } else if (i%3==0) {
+            } else if (divisibleBy3) {
                 con = "Fizz";
-            } else if (i%5==0) {
+            } else if (divisibleBy5) {
                 con ="Buzz";
             } else {
                 con +=i;
